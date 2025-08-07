9a3a643a5b2fe12d389e2e4c01243aac6ebfb742
Graham Markall <gmarkall@nvidia.com>
2025-08-07 10:17:45 +0100

    Use wheel deps in Windows test

--- ci/test_wheel.ps1
+++ ci/test_wheel.ps1
@@ -28,7 +28,7 @@ $CUDA_VER_MAJOR = ($env:CUDA_VER -split '\.')[0] -join '.'
 rapids-logger "Install wheel with test dependencies"
 $package = Resolve-Path wheel\numba_cuda*.whl | Select-Object -ExpandProperty Path
 echo "Package path: $package"
-python -m pip install "$package[test,cu12]" "cuda-python==$CUDA_VER_MAJOR.*" "cuda-core==0.3.*"
+python -m pip install "${package}[cu${CUDA_VER_MAJOR},test-cu${CUDA_VER_MAJOR}]"
 
 $SYS_PREFIX = (python -c "import sys; print(sys.prefix)")
 
